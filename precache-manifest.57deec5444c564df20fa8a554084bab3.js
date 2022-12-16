791279d6fcc41ea8a8320d7b1817becb9f07d63f
haveneshb15 <31435036+haveneshb15@users.noreply.github.com>
2022-12-16 22:56:44 +0800

    Updates

---
+++ precache-manifest.57deec5444c564df20fa8a554084bab3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "25a0581f355ce0a75c58",
+    "url": "/static/css/main.803e64c8.chunk.css"
+  },
+  {
+    "revision": "25a0581f355ce0a75c58",
+    "url": "/static/js/main.25a0581f.chunk.js"
+  },
+  {
+    "revision": "de53d44062204e8b07c9",
+    "url": "/static/js/1.de53d440.chunk.js"
+  },
+  {
+    "revision": "4bee9bf557f6b6f9afe4",
+    "url": "/static/css/2.08f691d7.chunk.css"
+  },
+  {
+    "revision": "4bee9bf557f6b6f9afe4",
+    "url": "/static/js/2.4bee9bf5.chunk.js"
+  },
+  {
+    "revision": "65e80e578c76170a4129",
+    "url": "/static/js/runtime~main.65e80e57.js"
+  },
+  {
+    "revision": "52196284de1fcb5b044f001a75482dba",
+    "url": "/static/media/bootstrap-icons.52196284.woff"
+  },
+  {
+    "revision": "7f477633ddd12f84284654f2a2e89b8a",
+    "url": "/static/media/bootstrap-icons.7f477633.woff2"
+  },
+  {
+    "revision": "a389faa89aa7139b0ea0c6b122902762",
+    "url": "/index.html"
+  }
+];
